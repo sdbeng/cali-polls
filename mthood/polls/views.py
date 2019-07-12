a11df8e1d71feca636fb77a63bd45de2a6ce2f1d
Sergio Barrera <sdbeng@gmail.com>
2019-07-12 13:43:51 -0700

    Add try:except to catch 404- question  does not

--- mthood/polls/views.py
+++ mthood/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import loader
 
@@ -15,17 +15,28 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context={
-        'latest_question_list':latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context={
+#         'latest_question_list':latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question':question})
+
+    # return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at results of question %s."
