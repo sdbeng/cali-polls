b773d424fe3056fec3e6ae781590ddfe30d4bd15
Sergio Barrera <sdbeng@gmail.com>
2019-07-12 22:16:01 -0700

    last check before going into generic views system

--- mthood/polls/views.py
+++ mthood/polls/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
-from django.shortcuts import render, get_object_or_404
+
 from django.template import loader
 
 from .models import Choice, Question
