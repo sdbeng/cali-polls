2f4160c220f1bbe060368296c5b9e5fc5f20a319
Sergio Barrera <sdbeng@gmail.com>
2019-07-14 09:54:00 -0700

    Add css static polls index,check other

--- mthood/mthood/urls.py
+++ mthood/mthood/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from . import views
 
 urlpatterns = [
+    # path('', views.home),
+    path('products/', include('products.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
